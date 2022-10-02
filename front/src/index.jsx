ba85b899e5935c445fa41051835cbb9ea73e7407
a_kiritani <a.kiritani@skillengine.jp>
2022-10-02 15:07:35 +0900

    カレンダーボード作成

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CalendarBoard from "./components/CalendarBoard";
 
-const App = () => <di>hello react!!</di>;
+
+const App = () => (
+  <div>
+    <CalendarBoard />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
