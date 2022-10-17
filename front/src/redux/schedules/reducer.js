7d8ddcf0a0c533f47956181373d050655acbc2f6
a_kiritani <a.kiritani@skillengine.jp>
2022-10-17 23:31:41 +0900

    スケジュールreducerの追加

--- front/src/redux/schedules/reducer.js
+++ front/src/redux/schedules/reducer.js
@@ -1,19 +1,29 @@
-import { SCHEDULES_ADD_ITEM } from "./actions";
 import dayjs from "dayjs";
-
+import {
+  SCHEDULES_ADD_ITEM,
+  SCHEDULES_FETCH_ITEM,
+  SCHEDULES_SET_LOADING
+} from "./actions";
 
 const init = {
-  items: [
-    {
-      id: 1,
-      title: "テスト",
-      date: dayjs(),
-      location: "会議室",
-      description: "経営戦略について"
-    }
-  ],
+  items: [],
   isLoading: false
 };
+
+
+// const init = {
+//   items: [
+//     {
+//       id: 1,
+//       title: "テスト",
+//       date: dayjs(),
+//       location: "会議室",
+//       description: "経営戦略について"
+//     }
+//   ],
+//   isLoading: false
+// };
+
 
 const schedulesReducer = (state = init, action) => {
   const { type, payload } = action;
@@ -24,6 +34,18 @@ const schedulesReducer = (state = init, action) => {
         ...state,
         items: [...state.items, { ...payload, id: state.items.length + 1 }]
       };
+    case SCHEDULES_SET_LOADING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case SCHEDULES_FETCH_ITEM:
+      return {
+        ...state,
+        isLoading: false,
+        items: payload
+      };
+  
     default:
       return state;
   }
